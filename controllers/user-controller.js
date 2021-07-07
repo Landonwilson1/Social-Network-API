7c7d991369f2c22583fdaaebf4607bd42ecaf419
Landon Wilson <77355267+Landonwilson1@users.noreply.github.com>
2021-07-07 17:23:17 -0400

    Finally working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -36,10 +36,6 @@ const userController = {
 
     // POST /api/users
     // expected body:
-    // {
-    //     "username": "foo",
-    //     "email": "bar@baz.com"  // must follow the email format
-    // }
     createUser({ body }, res) {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
@@ -47,11 +43,6 @@ const userController = {
     },
 
     // PUT /api/users/:id
-    // expected body includes at least one of the attributes below:
-    // {
-    //     "username": "foo",
-    //     "email": "bar@baz.com"  // must follow the email format
-    // }
     updateUser({ params, body }, res) {
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
         .then(dbUserData => {
@@ -66,20 +57,17 @@ const userController = {
 
     // DELETE /api/users/:id
     deleteUser({ params }, res) {
-        // delete the user
         User.findOneAndDelete({ _id: params.id })
         .then(dbUserData => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'No user found with this id'});
                 return;
             }
-            // remove the user from any friends arrays
             User.updateMany(
                 { _id : {$in: dbUserData.friends } },
                 { $pull: { friends: params.id } }
             )
             .then(() => {
-                // remove any comments from this user
                 Thought.deleteMany({ username : dbUserData.username })
                 .then(() => {
                     res.json({message: "Successfully deleted user"});
@@ -93,7 +81,6 @@ const userController = {
 
     // POST /api/users/:userId/friends/:friendId
     addFriend({ params }, res) {
-        // add friendId to userId's friend list
         User.findOneAndUpdate(
             { _id: params.userId },
             { $addToSet: { friends: params.friendId } },
@@ -104,7 +91,6 @@ const userController = {
                 res.status(404).json({ message: 'No user found with this userId' });
                 return;
             }
-            // add userId to friendId's friend list
             User.findOneAndUpdate(
                 { _id: params.friendId },
                 { $addToSet: { friends: params.userId } },
@@ -124,7 +110,6 @@ const userController = {
 
     // DELETE /api/users/:userId/friends/:friendId
     deleteFriend({ params }, res) {
-        // remove friendId from userId's friend list
         User.findOneAndUpdate(
             { _id: params.userId },
             { $pull: { friends: params.friendId } },
@@ -135,7 +120,6 @@ const userController = {
                 res.status(404).json({ message: 'No user found with this userId' });
                 return;
             }
-            // remove userId from friendId's friend list
             User.findOneAndUpdate(
                 { _id: params.friendId },
                 { $pull: { friends: params.userId } },
